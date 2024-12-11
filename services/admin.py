11618f9ab68f84c23b8025e16a50affde89c268c
Amir Dashti <ahdashti@gmail.com>
2024-12-11 15:31:07 +0330

    feat(admin): register Service and OilChange models with custom admin options
    Registered the Service and OilChange models in the Django admin interface with enhanced customization for improved usability. This includes setting autocomplete fields, defining list displays, adding search capabilities, and enabling filtering by oil type for the OilChange model. These changes aim to facilitate easier management and retrieval of service records within the admin panel.

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Service)
+class ServiceAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['user', 'vehicle']
+    list_display = ['id', 'user', 'vehicle',
+                    'service_date', 'mileage', 'description']
+    search_fields = ['user', 'vehicle', 'service_date', 'mileage']
+
+
+@admin.register(models.OilChange)
+class OilChangeAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['service']
+    list_display = ['service', 'oil_type', 'oil_lifetime_distance',
+                    'next_change_mileage', 'next_service_date', 'description']
+    list_filter = ['oil_type']
+    search_fields = ['oil_type']
